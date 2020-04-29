d89cf46027b62ec27bc0f111904dbf9ae1be19cf
bokismoki <borisbosnjak084@gmail.com>
2020-04-29 14:16:33 +0200

    Removed image lazy load from NewsItem

--- components/NewsItem.vue
+++ components/NewsItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="news-item flex h-20 p-2 transition-transform duration-200 smd:h-32 md:h-48" :class="news.bgColor">
-    <img class="m-auto max-w-full max-h-full" :src="news.logo" alt="News Logo" />
+    <img data-not-lazy class="m-auto max-w-full max-h-full" :src="news.logo" alt="News Logo" />
   </div>
 </template>
 
